7c6edcda474f0c73a752004f1f06d749cdfdc913
seraya <takahser@students.zhaw.ch>
2016-02-12 15:58:02 +0100

    created new (empty aspnet) project

---
+++ C-ShirtsNet/Modules/HomeModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Nancy;
+
+namespace CShirtsNet
+{
+	public class HomeModule : NancyModule
+	{
+		public HomeModule ()
+		{
+			Get["/"] = parameters => {
+				return View["Index"];
+			};
+		}
+		public static void Main()
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
+
