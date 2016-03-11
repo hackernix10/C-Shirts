f23ef5c31944e518823b468e25e365d7b49f5ef3
seraya <takahser@students.zhaw.ch>
2016-03-11 10:04:05 +0100

    mocked some tshirts, implemented master-detail

--- Frontend/app/app.component.js
+++ Frontend/app/app.component.js
@@ -22,22 +22,25 @@ System.register(['angular2/core'], function(exports_1, context_1) {
             AppComponent = (function () {
                 function AppComponent() {
                     this.title = 'Custom T-Shirts';
-                    this.tshirt = {
-                        id: 1,
-                        title: 'tell your story',
-                        printTechnique: 'screening'
-                    };
+                    this.tshirts = [
+                        { "id": 1, "title": "tell your story", "printTechnique": "Screening" },
+                        { "id": 2, "title": "ZRK", "printTechnique": "Sublimation" },
+                        { "id": 3, "title": "IAZ", "printTechnique": "DOG Print" },
+                        { "id": 4, "title": "Zara black", "printTechnique": "Screening" }
+                    ];
                 }
+                AppComponent.prototype.onSelect = function (tshirt) { this.selectedTShirt = tshirt; };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n        <h1>{{title}}</h1>\n        <h2>{{tshirt.title}} details!</h2>\n        <div><label>id: </label>{{tshirt.id}}</div>\n        <div>\n            <label>title: </label>\n            <div><input [(ngModel)]=\"tshirt.title\" placeholder=\"name\"></div>\n        </div>\n        <div>\n            <label>print technique: </label>\n            <div><input value=\"{{tshirt.printTechnique}}\" placeholder=\"printTechnique\"></div>\n        </div>\n        "
+                        template: "\n        <h1>{{title}}</h1>\n        <h2>My T-Shirts</h2>\n        <ul class=\"heroes\">\n            <li *ngFor=\"#tshirt of tshirts\"\n            [class.selected]=\"tshirt === selectedTShirt\"\n            (click)=\"onSelect(tshirt)\">\n                <span class=\"badge\">{{tshirt.id}}</span> {{tshirt.title}}\n            </li>\n        </ul>\n        <div *ngIf=\"selectedTShirt\">\n            <h2>{{selectedTShirt.title}} details!</h2>\n            <div><label>id: </label>{{selectedTShirt.id}}</div>\n            <div>\n                <label>title: </label>\n                <div><input [(ngModel)]=\"selectedTShirt.title\" placeholder=\"name\"></div>\n            </div>\n            <div>\n                <label>print technique: </label>\n                <div><input value=\"{{selectedTShirt.printTechnique}}\" placeholder=\"printTechnique\"></div>\n            </div>\n        </div>\n        ",
+                        styles: ["\n        .selected {\n            background-color: #CFD8DC !important;\n            color: white;\n        }\n        .heroes {\n            margin: 0 0 2em 0;\n            list-style-type: none;\n            padding: 0;\n            width: 10em;\n        }\n        .heroes li {\n            cursor: pointer;\n            position: relative;\n            left: 0;\n            background-color: #EEE;\n            margin: .5em;\n            padding: .3em 0;\n            height: 1.6em;\n            border-radius: 4px;\n        }\n        .heroes li.selected:hover {\n            background-color: #BBD8DC !important;\n            color: white;\n        }\n        .heroes li:hover {\n            color: #607D8B;\n            background-color: #DDD;\n            left: .1em;\n        }\n        .heroes .text {\n            position: relative;\n            top: -3px;\n        }\n        .heroes .badge {\n            display: inline-block;\n            font-size: small;\n            color: white;\n            padding: 0.8em 0.7em 0 0.7em;\n            background-color: #607D8B;\n            line-height: 1em;\n            position: relative;\n            left: -1px;\n            top: -4px;\n            height: 1.8em;\n            margin-right: .8em;\n            border-radius: 4px 0 0 4px;\n        }\n        "]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             }());
-            exports_1("AppComponent", AppComponent); // implement behaviour for view (properties & methods)
+            exports_1("AppComponent", AppComponent);
         }
     }
 });
--- Frontend/app/app.component.ts
+++ Frontend/app/app.component.ts
@@ -1,33 +1,96 @@
 import {Component} from 'angular2/core';
 
+interface TShirt {
+    id: number;
+    title: string;
+    printTechnique: string;
+}
+
 // creating a visual component 'AppComponent'
 @Component({
     selector: 'my-app', // CSS selector for a host HTML element named my-app
     template:`
         <h1>{{title}}</h1>
-        <h2>{{tshirt.title}} details!</h2>
-        <div><label>id: </label>{{tshirt.id}}</div>
-        <div>
-            <label>title: </label>
-            <div><input [(ngModel)]="tshirt.title" placeholder="name"></div>
+        <h2>My T-Shirts</h2>
+        <ul class="heroes">
+            <li *ngFor="#tshirt of tshirts"
+            [class.selected]="tshirt === selectedTShirt"
+            (click)="onSelect(tshirt)">
+                <span class="badge">{{tshirt.id}}</span> {{tshirt.title}}
+            </li>
+        </ul>
+        <div *ngIf="selectedTShirt">
+            <h2>{{selectedTShirt.title}} details!</h2>
+            <div><label>id: </label>{{selectedTShirt.id}}</div>
+            <div>
+                <label>title: </label>
+                <div><input [(ngModel)]="selectedTShirt.title" placeholder="name"></div>
+            </div>
+            <div>
+                <label>print technique: </label>
+                <div><input value="{{selectedTShirt.printTechnique}}" placeholder="printTechnique"></div>
+            </div>
         </div>
-        <div>
-            <label>print technique: </label>
-            <div><input value="{{tshirt.printTechnique}}" placeholder="printTechnique"></div>
-        </div>
-        `
+        `,
+    styles:[`
+        .selected {
+            background-color: #CFD8DC !important;
+            color: white;
+        }
+        .heroes {
+            margin: 0 0 2em 0;
+            list-style-type: none;
+            padding: 0;
+            width: 10em;
+        }
+        .heroes li {
+            cursor: pointer;
+            position: relative;
+            left: 0;
+            background-color: #EEE;
+            margin: .5em;
+            padding: .3em 0;
+            height: 1.6em;
+            border-radius: 4px;
+        }
+        .heroes li.selected:hover {
+            background-color: #BBD8DC !important;
+            color: white;
+        }
+        .heroes li:hover {
+            color: #607D8B;
+            background-color: #DDD;
+            left: .1em;
+        }
+        .heroes .text {
+            position: relative;
+            top: -3px;
+        }
+        .heroes .badge {
+            display: inline-block;
+            font-size: small;
+            color: white;
+            padding: 0.8em 0.7em 0 0.7em;
+            background-color: #607D8B;
+            line-height: 1em;
+            position: relative;
+            left: -1px;
+            top: -4px;
+            height: 1.8em;
+            margin-right: .8em;
+            border-radius: 4px 0 0 4px;
+        }
+        `]
 })
 export class AppComponent {
-    public title = 'Custom T-Shirts';
-    public tshirt: TShirt = {
-        id: 1,
-        title: 'tell your story',
-        printTechnique: 'screening'
-    }
+    title = 'Custom T-Shirts';
+    tshirts:  TShirt[] = [
+        {"id": 1, "title": "tell your story", "printTechnique": "Screening"},
+        {"id": 2, "title": "ZRK", "printTechnique": "Sublimation"},
+        {"id": 3, "title": "IAZ", "printTechnique": "DOG Print"},
+        {"id": 4, "title": "Zara black", "printTechnique": "Screening"}
+    ];
+    selectedTShirt: TShirt;
     
-} // implement behaviour for view (properties & methods)
-interface TShirt {
-    id: number;
-    title: string;
-    printTechnique: string;
+    onSelect(tshirt: TShirt) { this.selectedTShirt = tshirt; }
 }
